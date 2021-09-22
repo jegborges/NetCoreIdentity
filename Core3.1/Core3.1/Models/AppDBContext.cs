0f8f86b44142ee210ed00eb7985bfc655eed57ce
Emilio <emiliogarb@hotmail.com>
2021-09-22 15:20:38 -0500

    First Commit

---
+++ Core3.1/Core3.1/Models/AppDBContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Core3._1.Models
+{
+    public class AppDBContext : IdentityDbContext
+    {
+        private readonly DbContextOptions _options;
+
+        public AppDBContext(DbContextOptions options) : base(options)
+        {
+            _options = options;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+}
---
+++ Core3.1/Core3.1/Models/RegisterViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Core3._1.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "Password and confirmation password not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
